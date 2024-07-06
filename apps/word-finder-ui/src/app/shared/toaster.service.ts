b3dfdda29e378a2ef1daec7de8b0e5f2e8bf739e
mgraphic <mgraphic@users.noreply.github.com>
2024-07-05 23:28:40 -0400

    Fix ui app styles

--- apps/word-finder-ui/src/app/shared/toaster.service.ts
+++ apps/word-finder-ui/src/app/shared/toaster.service.ts
@@ -2,46 +2,50 @@ import { Injectable, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ToasterService {
-  static readonly DEFAULT_DURATION = 5000;
+    static readonly DEFAULT_DURATION = 5000;
 
-  private readonly snackBar = inject(MatSnackBar);
+    private readonly snackBar = inject(MatSnackBar);
 
-  success(
-    options: string | { message: string; action?: string; duration?: number },
-  ) {
-    this.openSnackBar(
-      typeof options === 'string'
-        ? { message: options, className: 'snackbar-success' }
-        : { ...options, className: 'snackbar-success' },
-    );
-  }
+    success(
+        options:
+            | string
+            | { message: string; action?: string; duration?: number }
+    ) {
+        this.openSnackBar(
+            typeof options === 'string'
+                ? { message: options, className: 'snackbar-success' }
+                : { ...options, className: 'snackbar-success' }
+        );
+    }
 
-  error(
-    options: string | { message: string; action?: string; duration?: number },
-  ) {
-    this.openSnackBar(
-      typeof options === 'string'
-        ? { message: options, className: 'snackbar-error' }
-        : { ...options, className: 'snackbar-error' },
-    );
-  }
+    error(
+        options:
+            | string
+            | { message: string; action?: string; duration?: number }
+    ) {
+        this.openSnackBar(
+            typeof options === 'string'
+                ? { message: options, className: 'snackbar-error' }
+                : { ...options, className: 'snackbar-error' }
+        );
+    }
 
-  private openSnackBar(options: {
-    message: string;
-    className: string;
-    action?: string;
-    duration?: number;
-  }) {
-    const { message, action, duration } = options;
+    private openSnackBar(options: {
+        message: string;
+        className: string;
+        action?: string;
+        duration?: number;
+    }) {
+        const { message, action, duration } = options;
 
-    this.snackBar.open(message, action, {
-      duration: duration || ToasterService.DEFAULT_DURATION,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-      panelClass: ['custom-snackbar', options.className],
-    });
-  }
+        this.snackBar.open(message, action, {
+            duration: duration || ToasterService.DEFAULT_DURATION,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            panelClass: ['custom-snackbar', options.className],
+        });
+    }
 }
